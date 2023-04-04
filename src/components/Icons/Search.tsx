11824f5ada2c3636e6bcf82d3b6aa2d27e73d13f
RichardPickman <drezzerock@gmail.com>
2023-04-04 21:43:19 +0200

    add icons

---
+++ src/components/Icons/Search.tsx
@@ -0,0 +1,10 @@
+
+interface Props {
+    size?: number,
+}
+
+export const Search = ({ size = 32 }: Props) => (
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 15.56 15.562">
+        <path id="icon_close" data-name="icon close" d="M1367.53,39.407l-2.12,2.121-5.66-5.657-5.66,5.657-2.12-2.121,5.66-5.657-5.66-5.657,2.12-2.122,5.66,5.657,5.66-5.657,2.12,2.122-5.66,5.657Z" transform="translate(-1351.97 -25.969)"></path>
+    </svg>
+);
