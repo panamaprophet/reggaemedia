a8b5066ced34a9ada9d08b96849c99b8dd14b174
RichardPickman <drezzerock@gmail.com>
2023-04-05 19:31:12 +0200

    remove returns

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,18 +5,14 @@ export const Layout = ({ children }: { children: ReactNode }) => {
     return <div>{children}</div>;
 };
 
-export const Row = ({ children, className }: { children: ReactNode, className?: string }) => {
-    return (
-        <div className={['flex', className].join(' ')}>
-            {children}
-        </div>
-    );
-};
+export const Row = ({ children, className }: { children: ReactNode, className?: string }) => (
+    <div className={['flex', className].join(' ')}>
+        {children}
+    </div>
+)
 
-export const Column = ({ children, className }: { children: ReactNode, className?: string }) => {
-    return (
-        <div className={['flex flex-col', className].join(' ')}>
-            {children}
-        </div>
-    );
-};
+export const Column = ({ children, className }: { children: ReactNode, className?: string }) => (
+    <div className={['flex flex-col', className].join(' ')}>
+        {children}
+    </div>
+)
