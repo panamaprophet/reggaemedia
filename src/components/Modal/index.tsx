e6d6e628199ee5f6a9543e4e47cda5de751beb59
RichardPickman <drezzerock@gmail.com>
2023-04-06 14:37:34 +0200

    update Modal component

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,3 +1,4 @@
+import { cx } from '@/helpers';
 import { ReactNode, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
@@ -5,11 +6,12 @@ import { createPortal } from 'react-dom';
 interface Props {
     children: ReactNode;
     isOpen: boolean;
+    size: string,
     onClose?: () => void;
-    size: string;
+    className?: string;
 }
 
-export const Modal = ({ children, isOpen, size, onClose = () => { } }: Props) => {
+export const Modal = ({ children, isOpen, size, className = '', onClose = () => { } }: Props) => {
     useEffect(() => {
         document.body.style.overflow = isOpen ? 'hidden' : 'visible';
     }, [isOpen]);
@@ -27,9 +29,9 @@ export const Modal = ({ children, isOpen, size, onClose = () => { } }: Props) =>
     }
 
     return createPortal(
-        <div className={["fixed h-full w-full top-0", size].join(' ')}>
-            <div className="top-0 left-0 right-0 bottom-0 fixed" onClick={onClose} />
-            <div className="w-full fixed flex items-center justify-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">{children}</div>
+        <div className={cx("fixed h-full w-full top-0", size)}>
+            <div className="top-0 left-0 right-0 bottom-0 fixed bg-slate-50/95" onClick={onClose} />
+            <div className={cx("w-full fixed flex items-center justify-center", className)}>{children}</div>
         </div >,
         document.body
     );
