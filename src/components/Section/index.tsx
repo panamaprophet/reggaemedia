5b5713f5fdcb0206dfc7db47c73fb220956cc8eb
RichardPickman <drezzerock@gmail.com>
2023-04-06 17:26:09 +0200

    remove paddings from Section

---
+++ src/components/Section/index.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+import { Container } from "../Container";
+
+interface Props {
+    children: ReactNode,
+}
+
+export const Section = ({ children }: Props) => {
+    return (
+        <div className="flex justify-center w-full border-b border-b-slate-200">
+            <Container>
+                {children}
+            </Container>
+        </div>
+    );
+};
