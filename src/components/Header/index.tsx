b07853e69dcdde82cc07c7ce44e87ff711d03780
RichardPickman <drezzerock@gmail.com>
2023-04-06 16:28:37 +0200

    remove paragraph

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,24 +6,7 @@ import { Search } from "../Search";
 import { YouTube } from "../Icons/YouTube";
 import { VK } from "../Icons/VK";
 import { Instagram } from "../Icons/Instagram";
-import { cx } from "@/helpers";
 
-
-const MediaLink = ({
-    link,
-    children,
-    background
-}: {
-    link: string,
-    children: ReactNode,
-    background: string,
-}) => (
-    <Link href={link}>
-        <div className={cx('w-8 h-8 flex items-center justify-center rounded', background)}>
-            {children}
-        </div>
-    </Link>
-);
 
 interface Props {
     onSearch: (text: string) => void,
@@ -41,22 +24,22 @@ export const Header = ({ onSearch }: Props) => {
             <Row className="justify-between mt-2 mr-4 ml-4 p-2">
                 <List className="flex flex-row gap-12 items-center">
                     <Link className="text-xs uppercase" href="/" >
-                        <p>Статьи</p>
+                        Статьи
                     </Link>
                     <Link className="text-xs uppercase" href="/" >
-                        <p>Связаться с нами</p>
+                        Связаться с нами
                     </Link>
                 </List>
                 <div className="flex flex-row items-center gap-4">
-                    <MediaLink link={links.vk} background="bg-blue-500">
+                    <Link className={'w-8 h-8 flex items-center justify-center rounded bg-blue-500'} href={links.vk}>
                         <VK size={14} />
-                    </MediaLink>
-                    <MediaLink link={links.youtube} background="bg-red-500">
+                    </Link>
+                    <Link className={'w-8 h-8 flex items-center justify-center rounded bg-red-500'} href={links.vk}>
                         <YouTube size={14} />
-                    </MediaLink>
-                    <MediaLink link={links.instagram} background="bg-blue-500">
+                    </Link>
+                    <Link className={'w-8 h-8 flex items-center justify-center rounded bg-blue-500'} href={links.vk}>
                         <Instagram size={14} />
-                    </MediaLink>
+                    </Link>
                     <Search onSubmit={(query: string) => onSearch(query)} />
                 </div>
             </Row>
