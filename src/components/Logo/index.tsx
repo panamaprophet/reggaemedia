465ac642954a373c6d8196ad05afc049541cfe84
RichardPickman <drezzerock@gmail.com>
2023-04-07 17:18:17 +0200

    rename folder and component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,9 @@
+import { Logo as Icon } from '../Icons/Logo';
+
+export const Logo = () => {
+    return (
+        <div className="flex items-center justify-center my-8">
+            <Icon size={130} />
+        </div>
+    );
+};
