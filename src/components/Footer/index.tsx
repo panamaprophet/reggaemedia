4a3efcbe5e303cf44146fc56ca282ad5aa4b33da
RichardPickman <drezzerock@gmail.com>
2023-04-07 13:00:04 +0200

    use getCurrentYear instead of hardcoding year

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,7 @@
+import { getCurrentYear } from "@/helpers";
+
 export const Footer = () => (
     <div className="flex items-center justify-center m-5">
-        <div className="text-xs uppercase text-gray-600">2023 © Reggae.Media. Crafted with love. </div>
+        <div className="text-xs uppercase text-gray-600">{getCurrentYear()} © Reggae.Media. Crafted with love. </div>
     </div>
 )
