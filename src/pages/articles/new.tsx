aadf5a8076393091b7047e9cbac4e5c4076a88ca
RichardPickman <drezzerock@gmail.com>
2023-04-13 20:59:39 +0200

    move editor related code to Editor itself

--- src/pages/articles/new.tsx
+++ src/pages/articles/new.tsx
@@ -1,79 +1,26 @@
-import { EditorState } from 'lexical';
-import { useEffect, useRef } from 'react';
+import Editor from "@/components/Editor";
+import { EditorState } from "lexical";
+import { useRef } from "react";
 
-import { LexicalComposer } from '@lexical/react/LexicalComposer';
-import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
-import { ContentEditable } from '@lexical/react/LexicalContentEditable';
-import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
-import { LinkPlugin } from '@lexical/react/LexicalLinkPlugin';
-import { ListPlugin } from '@lexical/react/LexicalListPlugin';
-import { TreeView } from '@lexical/react/LexicalTreeView';
-import { HeadingNode, QuoteNode } from "@lexical/rich-text";
-import { ListItemNode, ListNode } from "@lexical/list";
-import { AutoLinkNode, LinkNode } from "@lexical/link";
-import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
-import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
-import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+export const NewArticle = () => {
+    const ref = useRef<EditorState>();
+    const tags = ['qwe', 'asd', 'zxc'];
 
-
-function MyCustomAutoFocusPlugin() {
-    const [editor] = useLexicalComposerContext();
-
-    useEffect(() => {
-        // Focus the editor when the effect fires!
-        editor.focus();
-    }, [editor]);
-
-    return null;
-}
-
-function onError(error: Error) {
-    console.error(error);
-}
-
-function Editor() {
-    const editorStateRef = useRef<EditorState>();
-    const tags = ['asd', 'dsa', 'qwe']
-
-    const initialConfig = {
-        namespace: 'MyEditor',
-        onError,
-        nodes: [
-            HeadingNode,
-            HeadingNode,
-            QuoteNode,
-            ListItemNode,
-            ListNode,
-            AutoLinkNode,
-            LinkNode,
-        ]
-    };
+    const handleSave = () => console.log(JSON.stringify(ref))
 
     return (
-        <LexicalComposer initialConfig={initialConfig}>
-            <div className='flex flex-col items-center justify-evenly max-w-4xl bg-slate-600 gap-2'>
-                <input className="p-2 text-xl font-bold" type="text" placeholder="Start typing article header"></input>
-                <div className='flex gap-1 items-center justify-center'>
-                    {tags.map((tag) => <div key={tag + Math.random()}>{tag}</div>)}
-                    <input className="p-2 text-m" type="text" placeholder="Start typing tags"></input>
-                </div>
-                <div className="w-1/2 h-96 mt-8 bg-slate-300 rounded border">
-                    <RichTextPlugin
-                        contentEditable={<ContentEditable />}
-                        placeholder={<div>Enter some text...</div>}
-                        ErrorBoundary={LexicalErrorBoundary}
-                    />
-                    <OnChangePlugin onChange={editorState => editorStateRef.current = editorState} />
-                    <HistoryPlugin />
-                    <ListPlugin />
-                    <LinkPlugin />
-                    <MyCustomAutoFocusPlugin />
-                </div>
-                <button className="border p-2 rounded bg-slate-200" onClick={() => console.log(JSON.stringify(editorStateRef.current))}>Save</button>
+        <div className='flex self-center flex-col items-center justify-evenly max-w-4xl bg-slate-600 gap-2'>
+            <input className="p-2 text-xl font-bold" type="text" placeholder="Start typing article header"></input>
+            <div className='flex gap-1 items-center justify-center'>
+                {tags.map((tag) => <div key={tag + Math.random()}>{tag}</div>)}
+                <input className="p-2 text-m" type="text" placeholder="Start typing tags"></input>
             </div>
-        </LexicalComposer>
+            <div className="w-full mt-4 bg-white rounded border">
+                <Editor onChange={(state) => ref.current = state} />
+            </div>
+            <button className="border p-2 rounded bg-slate-200" onClick={handleSave}>Save</button>
+        </div>
     );
 }
 
-export default Editor;
-
+export default NewArticle;
