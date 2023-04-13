4481c8a727156d25cab3e548f24e72b044ee0612
RichardPickman <drezzerock@gmail.com>
2023-04-13 21:01:59 +0200

    add constants for toolbar

---
+++ src/components/Editor/settings/index.ts
@@ -0,0 +1,47 @@
+export const blockTypeToBlockName = {
+    bullet: 'Bulleted List',
+    check: 'Check List',
+    code: 'Code Block',
+    h1: 'Heading 1',
+    h2: 'Heading 2',
+    h3: 'Heading 3',
+    h4: 'Heading 4',
+    h5: 'Heading 5',
+    h6: 'Heading 6',
+    number: 'Numbered List',
+    paragraph: 'Normal',
+    quote: 'Quote',
+};
+
+export const rootTypeToRootName = {
+    root: 'Root',
+    table: 'Table',
+};
+
+export const FONT_FAMILY_OPTIONS: [string, string][] = [
+    ['Arial', 'Arial'],
+    ['Courier New', 'Courier New'],
+    ['Georgia', 'Georgia'],
+    ['Times New Roman', 'Times New Roman'],
+    ['Trebuchet MS', 'Trebuchet MS'],
+    ['Verdana', 'Verdana'],
+];
+
+export const FONT_SIZE_OPTIONS: [string, string][] = [
+    ['10px', '10px'],
+    ['11px', '11px'],
+    ['12px', '12px'],
+    ['13px', '13px'],
+    ['14px', '14px'],
+    ['15px', '15px'],
+    ['16px', '16px'],
+    ['17px', '17px'],
+    ['18px', '18px'],
+    ['19px', '19px'],
+    ['20px', '20px'],
+];
+
+export const dropDownActiveClass = (active: boolean) => {
+    if (active) return 'bg-gray-200';
+    else return '';
+}
