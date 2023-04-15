16e62027fc9e06f28938dc5ee5d1e97f7fc188db
RichardPickman <drezzerock@gmail.com>
2023-04-15 15:49:55 +0200

    move editor to its own folder

--- src/pages/editor/index.tsx
+++ src/pages/editor/index.tsx
@@ -1,26 +1,26 @@
-import Head from 'next/head';
+import Editor from "@/components/Editor";
+import { EditorState } from "lexical";
+import { useRef } from "react";
 
-/**
- * editor main page.
- *
- * shows the list of articles with quick actions
- * - publish / unpublish
- * - edit
- * - view
- * - delete
- * 
- * some optional website stuff editable by admin also should be located here
- *
- * @returns {React.Component>}
- */
-export default function Editor() {
+export const NewArticle = () => {
+    const ref = useRef<EditorState>();
+    const tags = ['qwe', 'asd', 'zxc'];
+
+    const handleSave = () => console.log(JSON.stringify(ref))
+
     return (
-        <>
-            <Head>
-                <title>Editor</title>
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-        </>
-    )
+        <div className='flex self-center flex-col items-center justify-evenly max-w-4xl bg-slate-600 gap-2'>
+            <input className="p-2 text-xl font-bold" type="text" placeholder="Start typing article header"></input>
+            <div className='flex gap-1 items-center justify-center'>
+                {tags.map((tag) => <div key={tag + Math.random()}>{tag}</div>)}
+                <input className="p-2 text-m" type="text" placeholder="Start typing tags"></input>
+            </div>
+            <div className="w-full mt-4 bg-white rounded border">
+                <Editor onChange={(state) => ref.current = state} />
+            </div>
+            <button className="border p-2 rounded bg-slate-200" onClick={handleSave}>Save</button>
+        </div>
+    );
 }
+
+export default NewArticle;
