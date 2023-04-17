b1e92b0d6d7dc05a1557a926b43ee5281d0799aa
RichardPickman <drezzerock@gmail.com>
2023-04-18 00:10:15 +0200

    add sizes for svgs and formatting reexport

--- src/components/Icons/Redo.tsx
+++ src/components/Icons/Redo.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export const Redo = ({ size = 32, color = '#000' }: Props) => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke={color}>
+        <svg width={size} height={size} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke={color}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M15 15l6-6m0 0l-6-6m6 6H9a6 6 0 000 12h3" />
         </svg>
     )
