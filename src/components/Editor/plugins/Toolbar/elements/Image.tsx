0330ee8bac5be0b411f7976f47a6d87d585ee3a3
RichardPickman <drezzerock@gmail.com>
2023-04-20 11:28:55 +0200

    remove old prop from Image element

--- src/components/Editor/plugins/Toolbar/elements/Image.tsx
+++ src/components/Editor/plugins/Toolbar/elements/Image.tsx
@@ -34,7 +34,6 @@ export const Image = () => {
             </Item>
 
             <Modal
-                className="top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2"
                 isOpen={isModalOpen}
                 onClose={() => setModalOpen(false)}
             >
