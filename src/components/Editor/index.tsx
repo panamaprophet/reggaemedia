68fab691be231c784f37a07e0e8c12f2ee3b2b18
RichardPickman <drezzerock@gmail.com>
2023-04-20 11:33:03 +0200

    add plugins to Editor

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,34 +1,21 @@
 import { EditorState } from 'lexical';
-import { useEffect } from 'react';
-
 import { LexicalComposer } from '@lexical/react/LexicalComposer';
 import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
 import { ContentEditable } from '@lexical/react/LexicalContentEditable';
 import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
 import { LinkPlugin } from '@lexical/react/LexicalLinkPlugin';
 import { ListPlugin } from '@lexical/react/LexicalListPlugin';
-
 import { HeadingNode, QuoteNode } from "@lexical/rich-text";
 import { ListItemNode, ListNode } from "@lexical/list";
 import { AutoLinkNode, LinkNode } from "@lexical/link";
 import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
 import { CheckListPlugin } from '@lexical/react/LexicalCheckListPlugin';
-import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
-import { ToolbarPlugin, ImagePlugin } from './plugins';
+
+import { ToolbarPlugin, ImagePlugin, FocusPlugin, TreeViewPlugin } from './plugins';
 
 import { ImageNode } from './plugins/Image/node';
 import theme from './theme';
-
-export const MyCustomAutoFocusPlugin = () => {
-    const [editor] = useLexicalComposerContext();
-
-    useEffect(() => {
-        editor.focus();
-    }, [editor]);
-
-    return null;
-}
 
 export const onError = (error: Error) => {
     console.error(error);
@@ -69,7 +56,8 @@ export const Editor = ({ onChange }: Props) => {
                 <LinkPlugin />
                 <ImagePlugin />
                 <CheckListPlugin />
-                <MyCustomAutoFocusPlugin />
+                <FocusPlugin />
+                <TreeViewPlugin />
             </div>
         </LexicalComposer>
     );
