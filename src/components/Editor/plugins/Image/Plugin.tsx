f8d1f46be7fc7f4026eb85e28b56edb2d62a164c
RichardPickman <drezzerock@gmail.com>
2023-04-22 19:04:24 +0200

    replace merRegister to useRegisterCommand hook

--- src/components/Editor/plugins/Image/Plugin.tsx
+++ src/components/Editor/plugins/Image/Plugin.tsx
@@ -1,46 +1,30 @@
-import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
-import { $wrapNodeInElement, mergeRegister } from '@lexical/utils';
+import { $wrapNodeInElement } from '@lexical/utils';
 import {
     $createParagraphNode,
     $insertNodes,
     $isRootOrShadowRoot,
     COMMAND_PRIORITY_EDITOR,
 } from 'lexical';
-import { useEffect } from 'react';
 
-import { $createImageNode, ImageNode } from './node';
+import { $createImageNode, ImagePayload } from './node';
 import { INSERT_IMAGE_COMMAND } from './command';
-import { InsertImagePayload } from './types';
+import { useRegisterCommand } from '../../hooks/useLexicalHooks';
 
 
-export const ImagePlugin = ({
-    captionsEnabled
-}: {
-    captionsEnabled?: boolean;
-}): JSX.Element | null => {
-    const [editor] = useLexicalComposerContext();
+export const ImagePlugin = (): JSX.Element | null => {
+    useRegisterCommand(
+        INSERT_IMAGE_COMMAND,
+        (payload: ImagePayload) => {
+            const imageNode = $createImageNode(payload);
+            $insertNodes([imageNode]);
+            if ($isRootOrShadowRoot(imageNode.getParentOrThrow())) {
+                $wrapNodeInElement(imageNode, $createParagraphNode).selectEnd();
+            }
 
-    useEffect(() => {
-        if (!editor.hasNodes([ImageNode])) {
-            throw new Error('ImagesPlugin: ImageNode not registered on editor');
-        }
-
-        return mergeRegister(
-            editor.registerCommand<InsertImagePayload>(
-                INSERT_IMAGE_COMMAND,
-                (payload) => {
-                    const imageNode = $createImageNode(payload);
-                    $insertNodes([imageNode]);
-                    if ($isRootOrShadowRoot(imageNode.getParentOrThrow())) {
-                        $wrapNodeInElement(imageNode, $createParagraphNode).selectEnd();
-                    }
-
-                    return true;
-                },
-                COMMAND_PRIORITY_EDITOR
-            )
-        );
-    }, [captionsEnabled, editor]);
+            return true;
+        },
+        COMMAND_PRIORITY_EDITOR
+    )
 
     return null;
 }
