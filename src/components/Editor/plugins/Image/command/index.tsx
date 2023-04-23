dffef09d693051f3edb875e188f2def5ab6408a0
RichardPickman <drezzerock@gmail.com>
2023-04-23 13:06:04 +0200

    small refactor and move types to node file

--- src/components/Editor/plugins/Image/command/index.tsx
+++ src/components/Editor/plugins/Image/command/index.tsx
@@ -1,5 +1,5 @@
 import { LexicalCommand, createCommand } from 'lexical';
-import { ImagePayload } from '../types';
+import { ImagePayload } from '../node';
 
 
 export const INSERT_IMAGE_COMMAND: LexicalCommand<ImagePayload> = createCommand(
