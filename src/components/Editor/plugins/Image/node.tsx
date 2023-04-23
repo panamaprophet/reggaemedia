ee70489446358d85a1cc2f2811de7c8b3feaafac
RichardPickman <drezzerock@gmail.com>
2023-04-23 18:45:45 +0200

    reduce amount of code in ImageNode

--- src/components/Editor/plugins/Image/node.tsx
+++ src/components/Editor/plugins/Image/node.tsx
@@ -1,14 +1,6 @@
-import type {
-    EditorConfig,
-    LexicalEditor,
-    LexicalNode,
-    SerializedEditor,
-    SerializedLexicalNode,
-    Spread,
-    NodeKey
-} from 'lexical';
+import type { EditorConfig, LexicalNode, SerializedLexicalNode, Spread, NodeKey } from 'lexical';
 
-import { createEditor, DecoratorNode } from 'lexical';
+import { DecoratorNode } from 'lexical';
 import { ImageComponent } from './Component';
 
 export type Dimension = 'inherit' | number;
@@ -20,22 +12,17 @@ export interface ImagePayload {
     width?: Dimension;
     height?: Dimension;
     maxWidth?: number;
-    showCaption?: boolean;
-    caption?: LexicalEditor;
-    captionsEnabled?: boolean;
 }
 
 type SerializedImageNode = Spread<
     {
-        altText: string;
-        caption: SerializedEditor;
-        height?: number;
-        maxWidth: number;
-        showCaption: boolean;
-        src: string;
-        width?: number;
-        type: 'image';
         version: 1;
+        type: 'image';
+        src: string;
+        altText: string;
+        maxWidth: number;
+        height?: number;
+        width?: number;
     },
     SerializedLexicalNode
 >;
@@ -53,15 +40,7 @@ const convertImageElement = (domNode: Node) => {
 }
 
 export class ImageNode extends DecoratorNode<JSX.Element> {
-    __src: string;
-    __altText: string;
-    __width: Dimension;
-    __height: Dimension;
-    __maxWidth: number;
-    __showCaption: boolean;
-    __caption: LexicalEditor;
-    // Captions cannot yet be used within editor cells
-    __captionsEnabled: boolean;
+    props: ImagePayload;
 
     static getType() {
         return 'image';
@@ -69,39 +48,26 @@ export class ImageNode extends DecoratorNode<JSX.Element> {
 
     static clone(node: ImageNode) {
         return new ImageNode({
-            src: node.__src,
-            altText: node.__altText,
-            maxWidth: node.__maxWidth,
-            width: node.__width || 'inherit',
-            height: node.__height || 'inherit',
-            showCaption: node.__showCaption,
-            caption: node.__caption,
-            captionsEnabled: node.__captionsEnabled,
-            key: node.__key
+            ...node.props,
+            width: node.props.width || 'inherit',
+            height: node.props.height || 'inherit',
         });
     }
 
     static importJSON(serializedNode: SerializedImageNode) {
-        const node = $createImageNode({
+        return new ImageNode({
             altText: serializedNode.altText,
             height: serializedNode.height,
             maxWidth: serializedNode.maxWidth,
-            showCaption: serializedNode.showCaption,
             src: serializedNode.src,
             width: serializedNode.width
         });
-        const nestedEditor = node.__caption;
-        const editorState = nestedEditor.parseEditorState(serializedNode.caption.editorState);
-        if (!editorState.isEmpty()) {
-            nestedEditor.setEditorState(editorState);
-        }
-        return node;
     }
 
     exportDOM() {
         const element = document.createElement('img');
-        element.setAttribute('src', this.__src);
-        element.setAttribute('alt', this.__altText);
+        element.setAttribute('src', this.props.src);
+        element.setAttribute('alt', this.props.altText);
         return { element };
     }
 
@@ -116,42 +82,24 @@ export class ImageNode extends DecoratorNode<JSX.Element> {
 
     constructor(props: ImagePayload) {
         super(props.key);
-        this.__src = props.src;
-        this.__altText = props.altText;
-        this.__maxWidth = props.maxWidth || 500;
-        this.__width = props.width || 'inherit';
-        this.__height = props.height || 'inherit';
-        this.__showCaption = props.showCaption || false;
-        this.__caption = props.caption || createEditor();
-        this.__captionsEnabled = props.captionsEnabled || props.captionsEnabled === undefined;
+        this.props = props;
     }
 
     exportJSON() {
         return {
-            altText: this.getAltText(),
-            caption: this.__caption.toJSON(),
-            height: this.__height === 'inherit' ? 0 : this.__height,
-            maxWidth: this.__maxWidth,
-            showCaption: this.__showCaption,
-            src: this.getSrc(),
-            type: 'image',
+            ...this.props,
             version: 1,
-            width: this.__width === 'inherit' ? 0 : this.__width
+            type: 'image',
+            width: this.props.width === 'inherit' ? 0 : this.props.width,
+            height: this.props.height === 'inherit' ? 0 : this.props.height,
         };
     }
 
     setWidthAndHeight(width: Dimension, height: Dimension) {
         const writable = this.getWritable();
-        writable.__width = width;
-        writable.__height = height;
+        writable.props.width = width;
+        writable.props.height = height;
     }
-
-    setShowCaption(showCaption: boolean) {
-        const writable = this.getWritable();
-        writable.__showCaption = showCaption;
-    }
-
-    // View
 
     createDOM(config: EditorConfig) {
         const span = document.createElement('span');
@@ -167,32 +115,19 @@ export class ImageNode extends DecoratorNode<JSX.Element> {
         return false;
     }
 
-    getSrc() {
-        return this.__src;
-    }
-
-    getAltText() {
-        return this.__altText;
-    }
-
     decorate() {
         return (
             <ImageComponent
-                src={this.__src}
-                altText={this.__altText}
-                width={this.__width}
-                height={this.__height}
-                maxWidth={this.__maxWidth}
+                src={this.props.src}
+                altText={this.props.altText}
+                width={this.props.width || 'inherit'}
+                height={this.props.height || 'inherit'}
+                maxWidth={this.props.maxWidth || 500}
                 nodeKey={this.getKey()}
-                showCaption={this.__showCaption}
-                caption={this.__caption}
-                captionsEnabled={this.__captionsEnabled}
                 resizable={true}
             />
         );
     }
 }
-
-export const $createImageNode = ({ ...props }: ImagePayload) => new ImageNode(props);
 
 export const $isImageNode = (node: LexicalNode | null | undefined): node is ImageNode => node instanceof ImageNode;
