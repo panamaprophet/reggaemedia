0563b1bfbf44926db003a0b6a6d27de5f15eee93
RichardPickman <drezzerock@gmail.com>
2023-04-24 17:10:45 +0200

    add Mutation listener and image ids storage for removing from bd

--- src/components/Editor/plugins/Image/Plugin.tsx
+++ src/components/Editor/plugins/Image/Plugin.tsx
@@ -1,22 +1,35 @@
 import { $wrapNodeInElement } from '@lexical/utils';
 import {
-    $createParagraphNode,
     $insertNodes,
     $isRootOrShadowRoot,
+    $createParagraphNode,
     COMMAND_PRIORITY_EDITOR,
 } from 'lexical';
 
 import { ImageNode, ImagePayload } from './node';
 import { INSERT_IMAGE_COMMAND } from './command';
-import { useRegisterCommand } from '../../hooks/useLexicalHooks';
+import { useRegisterCommand, useRegisterListener } from '../../hooks/useLexicalHooks';
+import { useState } from 'react';
 
+const mockRemove = (id: string) => console.log('Node with id:', id, 'successfully removed');
+
+type ImageInfo = {
+    id: string;
+    key: string;
+}
 
 export const ImagePlugin = (): JSX.Element | null => {
+    const [images, setImages] = useState<ImageInfo[]>([]);
+
     useRegisterCommand(
         INSERT_IMAGE_COMMAND,
         (payload: ImagePayload) => {
             const imageNode = new ImageNode(payload);
+            const imageInfo = { key: imageNode.__key, id: payload.id };
+
+            setImages((prev) => [...prev, imageInfo]);
             $insertNodes([imageNode]);
+
             if ($isRootOrShadowRoot(imageNode.getParentOrThrow())) {
                 $wrapNodeInElement(imageNode, $createParagraphNode).selectEnd();
             }
@@ -25,6 +38,24 @@ export const ImagePlugin = (): JSX.Element | null => {
         },
         COMMAND_PRIORITY_EDITOR
     );
+
+    useRegisterListener(
+        'onMutation',
+        [
+            ImageNode,
+            (mutatedNodes) => {
+                for (let [nodeKey, mutation] of mutatedNodes) {
+                    if (mutation === 'destroyed') {
+                        const imageInfo = images.find((item) => item.key === nodeKey);
+
+                        if (imageInfo) {
+                            mockRemove(imageInfo.id);
+                        }
+                    }
+                }
+            }
+        ]
+    )
 
     return null;
 }
