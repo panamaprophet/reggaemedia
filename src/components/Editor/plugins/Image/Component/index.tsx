4389351d011b1693684c3db5672debe443d50444
RichardPickman <drezzerock@gmail.com>
2023-04-26 14:47:34 +0200

    add image resizer for ImagePlugin

--- src/components/Editor/plugins/Image/Component/index.tsx
+++ src/components/Editor/plugins/Image/Component/index.tsx
@@ -1,5 +1,8 @@
-import type { NodeKey } from 'lexical';
-import { useRef } from 'react';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import { $getNodeByKey, NodeKey } from 'lexical';
+import { useEffect, useRef, useState, MouseEvent as SyntheticMouseEvent } from 'react';
+import { $isImageNode } from '../node';
+import { useDebouncer } from '@/hooks/useDebouncer';
 
 
 interface Props {
@@ -13,21 +16,79 @@ interface Props {
 }
 
 export const ImageComponent = (props: Props): JSX.Element => {
-    const { src, altText, width, height, maxWidth } = props;
-    const imageRef = useRef<null | HTMLImageElement>(null);
+    const [editor] = useLexicalComposerContext();
+    const [viewport, setViewport] = useState({ width: 500, height: 300 });
+    const debouncedViewport = useDebouncer(viewport, 500);
+    const [isSelected, setSelected] = useState(false);
+    const ref = useRef<null | HTMLImageElement>(null);
+
+    useEffect(() => {
+        const handleMouse = (event: MouseEvent) => {
+            if (ref.current?.contains(event.target as Node)) {
+                setSelected(true);
+            } else {
+                setSelected(false)
+            }
+        }
+
+        document.addEventListener('mousedown', handleMouse);
+
+        return () => document.removeEventListener('mousedown', handleMouse)
+    });
+
+    const handleSize = (event: SyntheticMouseEvent, dimension: 'width' | 'height') => {
+        const onMouseMove = (mouseMoveEvent: MouseEvent) => {
+            const startSize = viewport;
+            const startPosition = { x: event.clientX, y: event.clientY };
+
+            if (dimension === 'height') {
+                setViewport({
+                    ...viewport,
+                    height: startSize.height - startPosition.y + mouseMoveEvent.clientY,
+                });
+            } else {
+                setViewport({
+                    ...viewport,
+                    width: startSize.width - startPosition.x + mouseMoveEvent.clientX,
+                });
+            }
+        }
+
+        const onMouseUp = () => document.removeEventListener('mousemove', onMouseMove);
+
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', onMouseUp, { once: true });
+    }
+
+    useEffect(() => {
+        editor.update(() => {
+            const node = $getNodeByKey(props.nodeKey);
+
+            if ($isImageNode(node)) {
+                node.setWidthAndHeight(debouncedViewport.width, debouncedViewport.height);
+            }
+        })
+    }, [editor, debouncedViewport]);
 
     return (
-        <picture>
+        <picture ref={ref} className='relative block'>
             <img
-                src={src}
-                alt={altText}
-                ref={imageRef}
+                src={props.src}
+                alt={props.altText}
+                className={isSelected ? 'border-2 border-sky-600' : ''}
                 style={{
-                    height,
-                    maxWidth,
-                    width
+                    width: viewport.width,
+                    height: viewport.height,
                 }}
             />
+            {isSelected && (
+                <>
+                    <div onMouseDown={event => handleSize(event, 'height')} data-name="north" className="w-2 h-2 bg-sky-600 absolute -top-1 left-1/2 cursor-ns-resize" />
+                    <div onMouseDown={event => handleSize(event, 'width')} data-name="west" className="w-2 h-2 bg-sky-600 absolute -right-1 top-1/2 cursor-ew-resize" />
+                    <div onMouseDown={event => handleSize(event, 'height')} data-name="south" className="w-2 h-2 bg-sky-600 absolute -bottom-1 left-1/2 cursor-ns-resize" />
+                    <div onMouseDown={event => handleSize(event, 'width')} data-name="east" className="w-2 h-2 bg-sky-600 absolute -left-1 top-1/2 cursor-ew-resize" />
+                </>
+            )}
         </picture>
     );
 }
