cac3aa61c0c57e762972f5197f43c6706f295762
sergei klykov <pp@rawsound.ru>
2023-05-05 00:53:20 +0200

    preload article to editor

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,5 @@
 export const cx = (...args: string[]) => args.filter(Boolean).join(' ');
 
 export const getCurrentYear = () => new Date().getFullYear();
+
+export const isString = (value: any): value is string => typeof value === 'string';
--- src/pages/editor/[...id].tsx
+++ src/pages/editor/[...id].tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { GetServerSideProps } from 'next';
 import { getServerSession } from 'next-auth';
 import { SerializedEditorState } from 'lexical';
@@ -7,6 +7,8 @@ import { Editor } from '@/components/Editor';
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
+import { getArticleById } from '@/resolvers/articles';
+import { ParsedUrlQuery } from 'querystring';
 
 
 // returns the article id from the current location query
@@ -29,33 +31,22 @@ const useUserId = () => {
     ];
 };
 
+// returns the article id from parsed query string
+const getArticleId = (query: ParsedUrlQuery) => {
+    const { id: ids } = query;
+    const id = Array.isArray(ids) ? ids[0] : ids;
 
-export const Page = () => {
-    const [article, setArticle] = useState<SerializedEditorState>();
-    const [title, setTitle] = useState('');
-    const [tags, setTags] = useState<string[]>([]);
+    return id !== 'new' ? id : undefined;
+};
+
+
+export const Page = (props: { article: any }) => {
+    const [article, setArticle] = useState<SerializedEditorState>(props.article?.body);
+    const [title, setTitle] = useState(props.article?.title);
+    const [tags, setTags] = useState<string[]>(props.article?.tags ?? []);
     const [id, setArticleId] = useArticleId();
     const [authorId] = useUserId();
-    const [isLoading, setLoading] = useState(Boolean(id));
     // const [metadata, setMetadata] = useState({});
-
-    useEffect(() => {
-        if (id) {
-            fetch(`/api/articles/${id}`)
-                .then(response => response.json())
-                .then(({ article }) => {
-                    setTitle(article.title);
-                    setTags(article.tags);
-                    setArticle(article.body);
-
-                    // setMetadata({
-                    //     createdOn: article.createdOn,
-                    //     updatedOn: article.updatedOn,
-                    // });
-                })
-                .then(() => setLoading(false));
-        }
-    }, [id]);
 
     const save = async () => {
         // @todo: abstract
@@ -98,18 +89,10 @@ export const Page = () => {
                     className="max-w-full w-full focus:outline-none text-normal p-4"
                 />
 
-                {isLoading && (
-                    <div className="text-center text-normal p-4">
-                        загрузка...
-                    </div>
-                )}
-
-                {!isLoading && (
-                    <Editor
-                        initialState={article}
-                        onChange={state => setArticle(state?.toJSON())}
-                    />
-                )}
+                <Editor
+                    initialState={article}
+                    onChange={state => setArticle(state?.toJSON())}
+                />
             </div>
         </div>
     );
@@ -119,14 +102,21 @@ export default Page;
 
 export const getServerSideProps: GetServerSideProps<{}> = async (ctx) => {
     const session = await getServerSession(ctx.req, ctx.res, authOptions);
-
-    // @todo: consider the server side article fetching
+    const articleId = getArticleId(ctx.query);
 
     if (!session) {
         return {
             redirect: {
                 destination: `/api/auth/signin?callbackUrl=${ctx.req.url}`,
                 permanent: false,
+            },
+        };
+    }
+
+    if (articleId) {
+        return {
+            props: {
+                article: await getArticleById(articleId),
             },
         };
     }
