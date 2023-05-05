2bc62c7fbcf560da4eb036b065b2f10b301fa2f6
sergei klykov <pp@rawsound.ru>
2023-05-06 00:50:45 +0200

    put key out of every component

--- src/components/Editor/hooks/useEditorStateParser/index.tsx
+++ src/components/Editor/hooks/useEditorStateParser/index.tsx
@@ -22,50 +22,52 @@ export const useEditorStateParser = (
     let key = 0;
 
     const convertToHtml = (node: SerializedLexicalNode) => {
+        key++;
+
         if (isRoot(node)) {
             const children = node.children.map(convertToHtml);
 
-            return <div key={++key} className={theme.root}>{children}</div>;
+            return <div key={key} className={theme.root}>{children}</div>;
         }
 
         if (isParagraph(node)) {
             const children = node.children.map(convertToHtml);
             const align = getAlign(node.format);
 
-            return <p key={++key} className={cx(theme.paragraph, align)}>{children}</p>;
+            return <p key={key} className={cx(theme.paragraph, align)}>{children}</p>;
         }
 
         if (isQuote(node)) {
             const children = node.children.map(convertToHtml);
 
-            return <blockquote key={++key} className={theme.quote}>{children}</blockquote>;
+            return <blockquote key={key} className={theme.quote}>{children}</blockquote>;
         }
 
         if (isImage(node)) {
-            return <Image key={++key} {...node} alt={node.alt || ''} className={theme.image} />;
+            return <Image key={key} {...node} alt={node.alt || ''} className={theme.image} />;
         }
 
         if (isText(node)) {
             const style = getTextStyle(node.format);
 
-            return style ? <span key={++key} className={style}>{node.text}</span> : node.text;
+            return style ? <span key={key} className={style}>{node.text}</span> : node.text;
         }
 
         if (isLink(node)) {
             const children = node.children.map(convertToHtml);
 
-            return <a key={++key} href={node.url}>{children}</a>;
+            return <a key={key} href={node.url}>{children}</a>;
         }
 
         if (isHeading(node)) {
             const Tag = node.tag;
             const children = node.children.map(convertToHtml);
 
-            return <Tag key={++key} className={theme.heading?.[node.tag]}>{children}</Tag>;
+            return <Tag key={key} className={theme.heading?.[node.tag]}>{children}</Tag>;
         }
 
         if (isLineBreak(node)) {
-            return <br key={++key} />;
+            return <br key={key} />;
         }
 
         console.log('node is not supported:', node.type);
