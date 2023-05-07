a704b442c4d059b3f31b81175714bddbae2e1974
RichardPickman <drezzerock@gmail.com>
2023-05-07 18:32:32 +0200

    move logic from file uploader to command handler

--- src/components/Editor/plugins/Toolbar/elements/Uploaders.tsx
+++ src/components/Editor/plugins/Toolbar/elements/Uploaders.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { InputFile } from '@/components/Input/InputFile';
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
-import { INSERT_IMAGE_COMMAND } from '../../Image';
+import {
+    INSERT_IMAGE_URL_COMMAND,
+    INSERT_IMAGE_FILE_COMMAND,
+} from '../../Image';
 import { InputText } from '@/components/Input/InputText';
-import { uploadFile } from '@/resolvers/storage';
 
 
 interface Props {
@@ -13,32 +15,17 @@ interface Props {
 export const UploadFile = ({ onSubmit }: Props) => {
     const [editor] = useLexicalComposerContext();
     const [images, setImages] = useState<File[]>([]);
-    const [isLoading, setLoading] = useState(false);
 
-    const handleSumbit = async () => {
-        setLoading(true);
-        const promises = images.map((file) => uploadFile(file));
+    const handleSumbit = () => {
+        images.forEach((file) => editor.dispatchCommand(INSERT_IMAGE_FILE_COMMAND, file));
 
-        Promise.all(promises).then((urls) => {
-            urls.forEach((src) => {
-                if (!src) return;
-
-                const id = String(Math.random());
-
-                editor.dispatchCommand(INSERT_IMAGE_COMMAND, { id, src, alt: '' });
-            })
-
-            setLoading(false);
-            onSubmit();
-        })
+        onSubmit();
     };
 
     return (
         <div className='flex flex-col gap-2 bg-white rounded border items-center justify-center p-2'>
             <InputFile multiple onChange={(data) => Array.isArray(data) ? setImages(data) : setImages([data])} />
-            <button disabled={isLoading} className='w-full cursor-pointer border p-2' onClick={handleSumbit}>
-                {isLoading ? 'Загружается...' : 'Загрузить'}
-            </button>
+            <button disabled={images.length === 0} className='w-full cursor-pointer border p-2' onClick={handleSumbit}>Загрузить</button>
         </div>
     )
 }
@@ -48,9 +35,7 @@ export const UploadUrl = ({ onSubmit }: Props) => {
     const [url, setUrl] = useState<string>('');
 
     const handleSumbit = () => {
-        const id = String(Math.random());
-
-        editor.dispatchCommand(INSERT_IMAGE_COMMAND, { id, src: url, alt: '' });
+        editor.dispatchCommand(INSERT_IMAGE_URL_COMMAND, url);
 
         onSubmit();
     };
