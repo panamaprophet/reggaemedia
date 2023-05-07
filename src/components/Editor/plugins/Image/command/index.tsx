ae62b6279bd0182756a74ddfedefa4d7ab3a5b2c
RichardPickman <drezzerock@gmail.com>
2023-05-07 18:29:45 +0200

    add command for file and url uploads

--- src/components/Editor/plugins/Image/command/index.tsx
+++ src/components/Editor/plugins/Image/command/index.tsx
@@ -2,8 +2,12 @@ import { LexicalCommand, createCommand } from 'lexical';
 import { ImagePayload } from '../node';
 
 
-export const INSERT_IMAGE_COMMAND: LexicalCommand<ImagePayload> = createCommand(
-    'INSERT_IMAGE_COMMAND'
+export const INSERT_IMAGE_FILE_COMMAND: LexicalCommand<File> = createCommand(
+    'INSERT_IMAGE_FILE_COMMAND'
+);
+
+export const INSERT_IMAGE_URL_COMMAND: LexicalCommand<string> = createCommand(
+    'INSERT_IMAGE_URL_COMMAND'
 );
 
 export const RESIZE_IMAGE_COMMAND: LexicalCommand<{ width: number, height: number, key: ImagePayload['key'] }> = createCommand(
