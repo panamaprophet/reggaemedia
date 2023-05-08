76535fa9bb13705ee06c56048599f2d605ee9bff
RichardPickman <65208028+RichardPickman@users.noreply.github.com>
2023-05-08 12:14:25 +0000

    Page 404 (#25)
    * add 404page
    
    * switch function to arrow function
    
    * remove onHeader callback

---
+++ src/pages/404.tsx
@@ -0,0 +1,25 @@
+import { Header } from '@/components/Header';
+import { Link } from '@/components/Link';
+import { Section } from '@/components/Section';
+import Head from 'next/head';
+
+const Page = () => (
+    <>
+        <Head>
+            <title>Reggaemedia | Страница не найдена</title>
+        </Head>
+        <div className="relative h-screen w-screen">
+            <div className="absolute top-0 w-full">
+                <Section>
+                    <Header />
+                </Section>
+            </div>
+            <div className="flex items-center justify-center w-full h-full">
+                <h1 className="text-2xl font-extrabold text-slate-900 border-r pr-5">404</h1>
+                <h2 className="text-slate-700 ml-5">Такой страницы не существует. <Link className="text-blue-600" href="/">Домой?</Link></h2>
+            </div>
+        </div>
+    </>
+)
+
+export default Page;
