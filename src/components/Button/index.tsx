efb8b078bca8bdc38f55cc52c63b14d9a94ca80c
Sergei Klykov <pp@rawsound.ru>
2023-05-10 14:29:06 +0200

    add button size (#35)

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,66 +1,61 @@
+import { cx } from '@/helpers';
 import { ReactNode } from 'react';
 
 
-const ButtonTypes = {
+const ButtonType = {
     'default': `
         outline-none
         bg-none
         text-inherit
     `,
     'secondary': `
-        px-4
-        py-2
         font-semibold
-        text-sm
         bg-white
         text-slate-700
         border
         border-slate-300
-        rounded-md
-        shadow-sm
         focus:ring-4
         focus:ring-offset-2
         focus:ring-offset-slate-50
         focus:ring-blue-300
-        dark:bg-slate-700
-        dark:text-slate-200
-        dark:ring-offset-slate-900
-        dark:border-transparent
     `,
     'delete': `
-        px-4
-        py-2
         font-semibold
-        text-sm
         bg-red-500
         text-slate-100
         border
         border-red-200
-        rounded-md
-        shadow-sm
         focus:ring-4
         focus:ring-offset-2
         focus:ring-offset-slate-50
         focus:ring-blue-300
-        dark:bg-slate-700
-        dark:text-slate-200
-        dark:ring-offset-slate-900
-        dark:border-transparent
     `
 };
+
+const ButtonSize = {
+    'small': 'px-2 py-1 text-xs shadow-sm rounded',
+    'medium': 'px-4 py-2 text-sm shadow-sm rounded-md',
+}
 
 interface Props {
     children: ReactNode,
     onClick?: () => void,
-    type?: keyof typeof ButtonTypes,
+    type?: keyof typeof ButtonType,
+    size?: keyof typeof ButtonSize,
     to?: string,
 }
 
-export const Button = ({ type = 'default', children, to, onClick }: Props) => {
+export const Button = ({
+    type = 'default',
+    size = 'medium',
+    children,
+    to,
+    onClick,
+}: Props) => {
     const Tag = to ? 'a' : 'button';
 
     return (
-        <Tag className={`${ButtonTypes[type]}`} href={to} onClick={onClick}>
+        <Tag className={cx(ButtonSize[size], ButtonType[type])} href={to} onClick={onClick}>
             {children}
         </Tag>
     );
