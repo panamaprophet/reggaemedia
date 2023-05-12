249fd3a47c5befc2ffa195b483292c1003fdd3f7
Sergei Klykov <pp@rawsound.ru>
2023-05-12 11:18:44 +0200

    add some classes to tailwind safelist (#40)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './src/**/*.{js,ts,jsx,tsx}'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+    safelist: [
+        'text-left',
+        'text-right',
+        'text-center',
+        'text-justify',
+        'font-bold',
+        'font-normal',
+        'underline',
+        'underline-offset-1',
+        'italic',
+    ]
 }
 
