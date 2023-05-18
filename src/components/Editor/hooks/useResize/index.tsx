7f94cc481ef4ee6e48cf9b84ae7febda9452de56
RichardPickman <drezzerock@gmail.com>
2023-05-18 16:00:30 +0200

    reduce amount of arguments for useResize

--- src/components/Editor/hooks/useResize/index.tsx
+++ src/components/Editor/hooks/useResize/index.tsx
@@ -1,13 +1,10 @@
 import { useCallback, useEffect, MouseEvent as SyntheticMouseEvent, useState, ReactNode } from 'react';
-import { NodeKey } from 'lexical';
 
 interface Props {
     width: number,
     height: number,
-    nodeKey: NodeKey,
     isSelected: boolean,
-    allowCorners: boolean,
-    allowWidthAndHeight: boolean,
+    keepAspectRatio: boolean,
     callback: (width: number, height: number) => void,
 }
 
@@ -80,26 +77,30 @@ export const useResize = (props: Props): ReturnType => {
         }
     }, [isResizing, onResizeEnd, onResize]);
 
-    const Markers = !props.isSelected ? null : (
-        <>
-            {props.allowWidthAndHeight && (
-                <>
-                    <div onMouseDown={onResizeStart('height')} data-name="north" className="w-2 h-2 bg-sky-600 absolute -top-1 left-1/2 cursor-ns-resize" />
-                    <div onMouseDown={onResizeStart('width')} data-name="east" className="w-2 h-2 bg-sky-600 absolute -right-1 top-1/2 cursor-e-resize" />
-                    <div onMouseDown={onResizeStart('height')} data-name="south" className="w-2 h-2 bg-sky-600 absolute -bottom-1 left-1/2 cursor-s-resize" />
-                    <div onMouseDown={onResizeStart('width')} data-name="west" className="w-2 h-2 bg-sky-600 absolute -left-1 top-1/2 cursor-w-resize" />
-                </>
-            )}
-            {props.allowCorners && (
-                <>
-                    <div onMouseDown={onResizeStart('corner')} data-name="north-east" className="w-2 h-2 bg-sky-600 absolute -top-1 -right-1 cursor-ne-resize" />
-                    <div onMouseDown={onResizeStart('corner')} data-name="east-south" className="w-2 h-2 bg-sky-600 absolute -right-1 -bottom-1 cursor-se-resize" />
-                    <div onMouseDown={onResizeStart('corner')} data-name="south-west" className="w-2 h-2 bg-sky-600 absolute -left-1 -bottom-1 cursor-sw-resize" />
-                    <div onMouseDown={onResizeStart('corner')} data-name="west-north" className="w-2 h-2 bg-sky-600 absolute -top-1 -left-1 cursor-nw-resize" />
-                </>
-            )}
-        </>
-    )
+    let Markers = null;
+
+    if (props.isSelected) {
+        Markers = (
+            <>
+                {!props.keepAspectRatio && (
+                    <>
+                        <div onMouseDown={onResizeStart('height')} data-name="north" className="w-2 h-2 bg-sky-600 absolute -top-1 left-1/2 cursor-ns-resize" />
+                        <div onMouseDown={onResizeStart('width')} data-name="east" className="w-2 h-2 bg-sky-600 absolute -right-1 top-1/2 cursor-e-resize" />
+                        <div onMouseDown={onResizeStart('height')} data-name="south" className="w-2 h-2 bg-sky-600 absolute -bottom-1 left-1/2 cursor-s-resize" />
+                        <div onMouseDown={onResizeStart('width')} data-name="west" className="w-2 h-2 bg-sky-600 absolute -left-1 top-1/2 cursor-w-resize" />
+                    </>
+                )}
+                {props.keepAspectRatio && (
+                    <>
+                        <div onMouseDown={onResizeStart('corner')} data-name="north-east" className="w-2 h-2 bg-sky-600 absolute -top-1 -right-1 cursor-ne-resize" />
+                        <div onMouseDown={onResizeStart('corner')} data-name="east-south" className="w-2 h-2 bg-sky-600 absolute -right-1 -bottom-1 cursor-se-resize" />
+                        <div onMouseDown={onResizeStart('corner')} data-name="south-west" className="w-2 h-2 bg-sky-600 absolute -left-1 -bottom-1 cursor-sw-resize" />
+                        <div onMouseDown={onResizeStart('corner')} data-name="west-north" className="w-2 h-2 bg-sky-600 absolute -top-1 -left-1 cursor-nw-resize" />
+                    </>
+                )}
+            </>
+        )
+    }
 
     return [isResizing, Markers, { width, height }];
 }
