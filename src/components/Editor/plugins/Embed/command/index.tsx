a599fe979c6958bf9d413d1784b7dee80ad4e87d
RichardPickman <drezzerock@gmail.com>
2023-05-18 16:01:01 +0200

    reduce commands of embedPlugin

--- src/components/Editor/plugins/Embed/command/index.tsx
+++ src/components/Editor/plugins/Embed/command/index.tsx
@@ -1,13 +1,12 @@
 import { LexicalCommand, createCommand } from 'lexical';
-import { ImagePayload } from '../node';
 
 
 export const INSERT_IMAGE_FILE_COMMAND: LexicalCommand<File> = createCommand('INSERT_IMAGE_FILE_COMMAND');
 
 export const INSERT_IMAGE_URL_COMMAND: LexicalCommand<string> = createCommand('INSERT_IMAGE_URL_COMMAND');
 
-export const RESIZE_IMAGE_COMMAND: LexicalCommand<{ width: number, height: number, key: ImagePayload['key'] }> = createCommand(
-    'RESIZE_IMAGE_COMMAND'
+export const RESIZE_EMBED_COMMAND: LexicalCommand<{ width: number, height: number, key: string }> = createCommand(
+    'RESIZE_EMBED_COMMAND'
 );
 
 export const INSERT_EMBED_COMMAND: LexicalCommand<{ type: string, source: string } | { type: 'image', source: File | string }> = createCommand('INSERT_EMBED_COMMAND');
