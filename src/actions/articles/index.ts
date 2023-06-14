08a0f5be0e2eb759a4540b6f67e9c2e74a5e485f
RichardPickman <drezzerock@gmail.com>
2023-06-14 13:31:57 +0200

    omit method for get tags fetch

--- src/actions/articles/index.ts
+++ src/actions/articles/index.ts
@@ -44,4 +44,4 @@ export const publishArticle = (id: string) => fetch(`/api/articles/${id}/publish
 
 export const unpublishArticle = (id: string) => fetch(`/api/articles/${id}/unpublish`, { method: 'POST' }).then(response => response.json());
 
-export const getTags = () => fetch('/api/articles/tags/', { method: 'GET' }).then(response => response.json());
+export const getTags = () => fetch('/api/articles/tags/').then(response => response.json());
