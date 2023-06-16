069a8d7720e58916ea60812579d15312901fd281
sergei klykov <pp@rawsound.ru>
2023-06-16 13:08:13 +0200

    replace <a> with next/link in @/components/Link

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,18 +1,15 @@
+import { ComponentProps } from 'react';
+import NextLink from 'next/link';
 import { cx } from '@/helpers';
-import { ReactNode } from 'react';
 
 
-type Props = {
-    children: ReactNode,
-    className?: string,
-    to?: string,
-    target?: '_self' | '_blank',
-    onClick?: () => void,
-}
+type LinkProps = ComponentProps<typeof NextLink>;
+
+type Props = Omit<LinkProps, 'href'> & { to: LinkProps['href'] };
 
 
 export const Link = ({ className, children, to, ...props }: Props) => (
-    <a className={cx('hover:underline underline-offset-2 transition cursor-pointer', className)} href={to} {...props}>
+    <NextLink className={cx('hover:underline underline-offset-2 transition cursor-pointer', className)} href={to} {...props}>
         {children}
-    </a>
+    </NextLink>
 );
