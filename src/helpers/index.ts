f73c0bc7341efa2a396791caaea0e19b8c95555a
RichardPickman <drezzerock@gmail.com>
2023-06-17 16:21:19 +0200

    add getSelectedNode checker

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,4 +1,6 @@
 import { Article } from '@/types';
+import { $isAtNodeEnd } from '@lexical/selection';
+import { RangeSelection } from 'lexical';
 
 
 export const cx = (...classes: unknown[]) =>
@@ -14,3 +16,19 @@ export const sortByDate = (a: Article, b: Article) => (b.updatedOn || b.createdO
 export const removeByIndex = <T,>(index: number, items: T[]) => items.filter((_, itemIndex) => itemIndex !== index);
 
 export const setByIndex = <T,>(index: number, value: T, items: T[]) => items.map((item, itemIndex) => itemIndex === index ? value : item);
+
+export const getSelectedNode = (selection: RangeSelection) => {
+    const anchor = selection.anchor;
+    const focus = selection.focus;
+    const anchorNode = selection.anchor.getNode();
+    const focusNode = selection.focus.getNode();
+    if (anchorNode === focusNode) {
+        return anchorNode;
+    }
+    const isBackward = selection.isBackward();
+    if (isBackward) {
+        return $isAtNodeEnd(focus) ? anchorNode : focusNode;
+    } else {
+        return $isAtNodeEnd(anchor) ? anchorNode : focusNode;
+    }
+}
