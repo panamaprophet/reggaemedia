7155928ce120aa374a3ec0496d63e92f23d0f93a
RichardPickman <drezzerock@gmail.com>
2023-06-18 16:05:35 +0200

    move getSelectedNode

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,7 +1,4 @@
 import { Article } from '@/types';
-import { $isAtNodeEnd } from '@lexical/selection';
-import { RangeSelection } from 'lexical';
-
 
 export const cx = (...classes: unknown[]) =>
     classes
@@ -16,19 +13,3 @@ export const sortByDate = (a: Article, b: Article) => (b.updatedOn || b.createdO
 export const removeByIndex = <T,>(index: number, items: T[]) => items.filter((_, itemIndex) => itemIndex !== index);
 
 export const setByIndex = <T,>(index: number, value: T, items: T[]) => items.map((item, itemIndex) => itemIndex === index ? value : item);
-
-export const getSelectedNode = (selection: RangeSelection) => {
-    const anchor = selection.anchor;
-    const focus = selection.focus;
-    const anchorNode = selection.anchor.getNode();
-    const focusNode = selection.focus.getNode();
-    if (anchorNode === focusNode) {
-        return anchorNode;
-    }
-    const isBackward = selection.isBackward();
-    if (isBackward) {
-        return $isAtNodeEnd(focus) ? anchorNode : focusNode;
-    } else {
-        return $isAtNodeEnd(anchor) ? anchorNode : focusNode;
-    }
-}
