70481efdee5e704273a54af3355b58ed71a8a859
RichardPickman <drezzerock@gmail.com>
2023-06-21 13:29:27 +0200

    add floating state for Modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -7,25 +7,41 @@ const modalSizes = {
     'screen': 'w-screen h-screen'
 }
 
-interface Props {
-    children: ReactNode;
-    isOpen: boolean;
-    size?: 'auto' | 'screen',
-    onClose?: () => void;
-    className?: string;
+const modalType = {
+    'default': 'top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ',
+    'float': '',
 }
 
-export const Modal = ({ children, size = 'auto', isOpen, onClose = () => { } }: Props) => {
+const modalOverlay = {
+    'float': 'bg-transparent',
+    'default': 'bg-slate-50/95' 
+}
+
+interface Props {
+    children: ReactNode,
+    isOpen: boolean,
+    type?: 'default' | 'float',
+    size?: 'auto' | 'screen',
+    onClose?: () => void;
+    className?: string,
+    position?: { top: number, left: number },
+}
+
+export const Modal = ({ children, size = 'auto', type = 'default', isOpen, onClose = () => { }, position }: Props) => {
     useEffect(() => {
-        document.body.style.overflow = isOpen ? 'hidden' : 'visible';
-    }, [isOpen]);
+        if (type !== 'float') {
+            document.body.style.overflow = isOpen ? 'hidden' : 'visible';
+        }
+    }, [isOpen, type]);
 
     useEffect(() => {
         const handleKeyboardClick = (event: KeyboardEvent) => (event.code === 'Escape') && onClose();
 
         document.addEventListener('keydown', handleKeyboardClick);
 
-        return () => document.removeEventListener('keydown', handleKeyboardClick);
+        return () => {
+            document.removeEventListener('keydown', handleKeyboardClick);
+        }
     }, [onClose]);
 
     if (!isOpen) {
@@ -34,11 +50,11 @@ export const Modal = ({ children, size = 'auto', isOpen, onClose = () => { } }: 
 
     return createPortal(
         <div className="fixed h-full w-full top-0 left-0 right-0 bottom-0">
-            <div className="fixed h-full w-full bg-slate-50/95" onClick={onClose} />
-            <div className={`fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ${modalSizes[size]}`}>
+            <div className={`fixed h-full w-full ${modalOverlay[type]}`} onClick={onClose} />
+            <div className={`fixed ${modalSizes[size]} ${modalType[type]}`} style={position}>
                 {children}
             </div>
         </div >,
-        document.body
+        document.body,
     );
 };
