0e5cfbc60983703f2c75471ff02f04b893968aa8
sergei klykov <pp@rawsound.ru>
2023-07-12 15:41:45 +0200

    make resize more predictable

---
+++ src/components/Editor/hooks/useResize/Marker.tsx
@@ -0,0 +1,21 @@
+import { MouseEventHandler } from 'react';
+import { cx } from '@/helpers';
+
+
+type MarkerType = 'top-left' | 'top-right' | 'bottom-right' | 'bottom-left';
+
+const MarkerPositionMap: { [k in MarkerType]: string } = {
+    'top-left': '-top-1 -left-1 cursor-nw-resize',
+    'top-right': '-top-1 -right-1 cursor-ne-resize',
+    'bottom-left': '-left-1 -bottom-1 cursor-sw-resize',
+    'bottom-right': '-right-1 -bottom-1 cursor-se-resize',
+};
+
+
+export const Marker = ({ type, onClick }: { type: MarkerType, onClick: MouseEventHandler<HTMLDivElement> }) => (
+    <div
+        data-name={type}
+        onMouseDown={onClick}
+        className={cx('absolute w-2 h-2 bg-sky-600', MarkerPositionMap[type])}
+    />
+);
--- src/components/Editor/hooks/useResize/index.tsx
+++ src/components/Editor/hooks/useResize/index.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, MouseEvent as SyntheticMouseEvent, useState, ReactNode } from 'react';
+import { Marker } from './Marker';
 
 interface Props {
     width: number,
@@ -10,56 +11,74 @@ interface Props {
 
 type ReturnType = [boolean, ReactNode, { width: number, height: number }];
 
+
+const getDirection = <T extends { x: number, y: number },>(a: T, b: T) => {
+    if (a.x > b.x && a.y === b.y) return 'right';
+    if (a.x == b.x && a.y > b.y) return 'down';
+    if (a.x == b.x && a.y < b.y) return 'up';
+    if (a.x < b.x && a.y == b.y) return 'left';
+
+    return null;
+};
+
+
 export const useResize = (props: Props): ReturnType => {
     const [{ width, height }, setSize] = useState({ width: props.width, height: props.height });
     const [isResizing, setResizing] = useState(false);
-    const [dimension, setDimension] = useState<string | null>(null);
+    const [type, setType] = useState<string | null>(null);
     const [startPoint, setStartPoint] = useState({ x: 0, y: 0 });
     const callback = props.callback;
 
-    // mouse down
-    const onResizeStart = (value: string) => (event: SyntheticMouseEvent) => {
+    const onResizeStart = (event: SyntheticMouseEvent) => {
         setResizing(true);
-        setDimension(value);
+        setType(event.currentTarget.getAttribute('data-name'));
         setStartPoint({ x: event.clientX, y: event.clientY });
     };
 
-    // mouse move
-    const onResize = useCallback((event: MouseEvent) => {
+    const onResize = useCallback((event: any) => {
         const { clientX: x, clientY: y } = event;
-        const currentWidth = width + (x - startPoint.x);
-        const currentHeight = height + (y - startPoint.y);
+        const keepAspectRatio = event.altKey;
 
         let w = width;
         let h = height;
 
-        switch (dimension) {
-            case 'height':
-                h = currentHeight;
-                break;
-            case 'width':
-                w = currentWidth;
-                break;
-            default:
-                const ratio = width / height;
+        const direction = getDirection({ x, y }, startPoint);
 
-                if (currentWidth / ratio < currentHeight) {
-                    w = currentWidth;
-                    h = currentWidth / ratio;
-                } else {
-                    w = currentHeight * ratio;
-                    h = currentHeight;
-                }
+        const isLeft = type === 'top-left' || type === 'bottom-left';
+        const isTop = type === 'top-left' || type === 'top-right';
+        
+        const dx = Math.abs(x - startPoint.x);
+        const dy = Math.abs(y - startPoint.y);
+
+        switch (direction) {
+            case 'left':
+                w = isLeft ? width + dx : width - dx;
+                break;
+            case 'right':
+                w = isLeft ? width - dx : width + dx;
+                break;
+            case 'up':
+                h = isTop ? height + dy : height - dy;
+                break;
+            case 'down':
+                h = isTop ? height - dy : height + dy;
+                break;
+        }
+
+        if (keepAspectRatio && (direction === 'left' || direction === 'right')) {
+            h = h / (width / w);
+        }
+
+        if (keepAspectRatio && (direction === 'up' || direction === 'down')) {
+            w = w / (height / h);
         }
 
         setStartPoint({ x, y });
         setSize({ width: w, height: h });
-    }, [startPoint, dimension, height, width]);
+    }, [startPoint, type, height, width]);
 
-    // mouse up
     const onResizeEnd = useCallback(() => {
         setResizing(false);
-
         callback(width, height);
     }, [callback, width, height]);
 
@@ -82,22 +101,10 @@ export const useResize = (props: Props): ReturnType => {
     if (props.isSelected) {
         Markers = (
             <>
-                {!props.keepAspectRatio && (
-                    <>
-                        <div onMouseDown={onResizeStart('height')} data-name="north" className="w-2 h-2 bg-sky-600 absolute -top-1 left-1/2 cursor-ns-resize" />
-                        <div onMouseDown={onResizeStart('width')} data-name="east" className="w-2 h-2 bg-sky-600 absolute -right-1 top-1/2 cursor-e-resize" />
-                        <div onMouseDown={onResizeStart('height')} data-name="south" className="w-2 h-2 bg-sky-600 absolute -bottom-1 left-1/2 cursor-s-resize" />
-                        <div onMouseDown={onResizeStart('width')} data-name="west" className="w-2 h-2 bg-sky-600 absolute -left-1 top-1/2 cursor-w-resize" />
-                    </>
-                )}
-                {props.keepAspectRatio && (
-                    <>
-                        <div onMouseDown={onResizeStart('corner')} data-name="north-east" className="w-2 h-2 bg-sky-600 absolute -top-1 -right-1 cursor-ne-resize" />
-                        <div onMouseDown={onResizeStart('corner')} data-name="east-south" className="w-2 h-2 bg-sky-600 absolute -right-1 -bottom-1 cursor-se-resize" />
-                        <div onMouseDown={onResizeStart('corner')} data-name="south-west" className="w-2 h-2 bg-sky-600 absolute -left-1 -bottom-1 cursor-sw-resize" />
-                        <div onMouseDown={onResizeStart('corner')} data-name="west-north" className="w-2 h-2 bg-sky-600 absolute -top-1 -left-1 cursor-nw-resize" />
-                    </>
-                )}
+                <Marker onClick={onResizeStart} type="top-left" />
+                <Marker onClick={onResizeStart} type="top-right" />
+                <Marker onClick={onResizeStart} type="bottom-right" />
+                <Marker onClick={onResizeStart} type="bottom-left" />
             </>
         )
     }
