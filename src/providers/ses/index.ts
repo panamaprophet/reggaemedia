d77798fe47da017ec9ebb5e5781fcf59cac987ca
sergei klykov <pp@rawsound.ru>
2023-10-01 18:16:59 +0200

    remove custom aws environment variables in favor of default ones

--- src/providers/ses/index.ts
+++ src/providers/ses/index.ts
@@ -1,9 +1,3 @@
 import { SESClient } from '@aws-sdk/client-ses';
 
-export const client = new SESClient({
-    credentials: {
-        accessKeyId: String(process.env.AMAZON_ACCESS_KEY),
-        secretAccessKey: String(process.env.AMAZON_SECRET_KEY),
-    },
-    region: String(process.env.AMAZON_REGION),
-});
+export const client = new SESClient();
